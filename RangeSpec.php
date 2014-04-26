d480236e2eba97b96d6d685f8c6f37a33f91fdf1
Pondd <pond1982a@gmail.com>
2014-04-26 20:13:40 +0700

    rearrange test methods

--- RangeSpec.php
+++ RangeSpec.php
@@ -18,23 +18,17 @@ class RangeSpec extends PHPUnit_Framework_TestCase {
   function testOpenOpenRangeZeroToFive () {
     $this->assertEquals("{1,2,3,4}", calRange("(0,5)"));
   }
+
   function testOpenOpenRangeZeroToZero () {
     $this->assertEquals("{}", calRange("(0,0)"));
   }
+
   function testCloseCloseRangeOneToOne () {
     $this->assertEquals("{1}", calRange("[1,1]"));
   }
+
   function testOpenOpenRangeZeroToFour () {
     $this->assertEquals("{1,2,3}", calRange("(0,4)"));
-  }
-  function testCloseGetMember () {
-    $this->assertEquals("1,2,3", getCloseMembers("0","4"));
-  }
-  function testCloseGetMemberSixToTen () {
-    $this->assertEquals("7,8,9", getCloseMembers("6","10"));
-  }
-  function testCloseGetMemberOneToOne (){
-    $this->assertEquals("", getCloseMembers("1","1"));
   }
 
   function testCloseCloseRangeTwoToTwo() {
@@ -52,6 +46,24 @@ class RangeSpec extends PHPUnit_Framework_TestCase {
     } catch (Exception $e) {
       $this->assertTrue(true);
     }
+  }
+  
+  function testCloseGetMember () {
+    $this->assertEquals("1,2,3", getCloseMembers("0","4"));
+  }
+
+  function testCloseGetMemberSixToTen () {
+    $this->assertEquals("7,8,9", getCloseMembers("6","10"));
+  }
+
+  function testCloseGetMemberOneToOne (){
+    $this->assertEquals("", getCloseMembers("1","1"));
+  }
+  
+
+
+  function testAlwaysfail(){
+    //$this->assertTrue(fail);
   }
 
 
