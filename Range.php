df2fc929457ba8a0356c99761f9d1ae5e99b3592
roofimon <roofimon@gmail.com>
2014-04-26 14:43:54 +0700

    refactor 4 case

--- Range.php
+++ Range.php
@@ -4,16 +4,18 @@ function rangeZeroToFive($range) {
 
   $firstPosition = 0;
   $lastPosition = 4;
-  $openSignPosition = $range[$firstPosition];
-  $closeSignPosition = $range[$lastPosition];
+  $firstSign = $range[$firstPosition];
+  $lastSign = $range[$lastPosition];
 
-  if ($openSignPosition == "(" && $closeSignPosition == ")") {
+  $signs = $firstSign . $lastSign;
+
+  if ($signs == "()") {
     return "{1,2,3,4}";
-  }
-  else if($openSignPosition == "(")  {
+
+  } else if($firstSign == "(")  {
     return "{1,2,3,4,5}";
 
-  } else if($closeSignPosition == ")") {
+  } else if($lastSign == ")") {
     return "{0,1,2,3,4}";
 
   }
